1639003d82537dc23f354d367574fbcaafba64c6
Daniel Green <62838829+Green-D-683@users.noreply.github.com>
2026-02-09 23:50:26 +0000

    Change Python version for humble from 3.10 to 3.9

--- overlay.nix
+++ overlay.nix
@@ -11,7 +11,7 @@ let
         distro = "jazzy";
         rosPython =
           {
-            humble = final.python310;
+            humble = final.python39;
             jazzy = final.python312;
             kilted = final.python312;
             rolling = final.python312;
