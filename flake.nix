d41ab185231d6e7fb1b63b863eac15e0f8deda0a
Daniel Green <62838829+Green-D-683@users.noreply.github.com>
2026-02-09 23:55:32 +0000

    Update nixpkgs URL to version 24.11 - need python 3.9

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
